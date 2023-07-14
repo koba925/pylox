c63a9d782ff23270ca60271c2940319f7af96669
Kobayashi Takahiro <koba925@gmail.com>
2023-07-14 17:04:25 +0900

    Refactor test by parametrize

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,5 +1,6 @@
 import sys
-from typing import Optional
+from typing import Any
+import pytest
 
 from src.pylox.lox_token import TokenType
 from src.pylox.lox_scanner import Scanner
@@ -14,12 +15,33 @@ def test_null_source() -> None:
     assert token.literal is None
     assert token.line == 1
 
+single_tokens: list[tuple[str, TokenType, Any, int]] = [
+    ("(", TokenType.LEFT_PAREN, None, 1),
+    (")", TokenType.RIGHT_PAREN, None, 1),
+    ("{", TokenType.LEFT_BRACE, None, 1),
+    ("}", TokenType.RIGHT_BRACE, None, 1),
+    (",", TokenType.COMMA, None, 1),
+    (".", TokenType.DOT, None, 1),
+    ("-", TokenType.MINUS, None, 1),
+    ("+", TokenType.PLUS, None, 1),
+    (";", TokenType.SEMICOLON, None, 1),
+    ("*", TokenType.STAR, None, 1),
+    ("!=", TokenType.BANG_EQUAL, None, 1),
+    ("!", TokenType.BANG, None, 1),
+    ("==", TokenType.EQUAL_EQUAL, None, 1),
+    ("=", TokenType.EQUAL, None, 1),
+    ("<=", TokenType.LESS_EQUAL, None, 1),
+    ("<", TokenType.LESS, None, 1),
+    (">=", TokenType.GREATER_EQUAL, None, 1),
+    (">", TokenType.GREATER, None, 1)
+]
 
-def __test_single_token(
+@pytest.mark.parametrize("source, token_type, literal, line", single_tokens)
+def test_single_token(
     source: str,
     token_type: TokenType,
-    literal: Optional[str] = None,
-    line: Optional[int] = None,
+    literal: Any,
+    line: int,
 ) -> None:
     print(source, token_type, literal, line, file=sys.stderr)
     scanner = Scanner(source)
@@ -31,30 +53,3 @@ def __test_single_token(
     assert token.lexeme == source
     assert token.literal == literal
     assert token.line == line
-
-
-def test_single_tokens() -> None:
-    single_tokens: list[tuple[str, TokenType]] = [
-        ("(", TokenType.LEFT_PAREN),
-        ("(", TokenType.LEFT_PAREN),
-        (")", TokenType.RIGHT_PAREN),
-        ("{", TokenType.LEFT_BRACE),
-        ("}", TokenType.RIGHT_BRACE),
-        (",", TokenType.COMMA),
-        (".", TokenType.DOT),
-        ("-", TokenType.MINUS),
-        ("+", TokenType.PLUS),
-        (";", TokenType.SEMICOLON),
-        ("*", TokenType.STAR),
-        ("!=", TokenType.BANG_EQUAL),
-        ("!", TokenType.BANG),
-        ("==", TokenType.EQUAL_EQUAL),
-        ("=", TokenType.EQUAL),
-        ("<=", TokenType.LESS_EQUAL),
-        ("<", TokenType.LESS),
-        (">=", TokenType.GREATER_EQUAL),
-        (">", TokenType.GREATER)
-    ]
-
-    for token in single_tokens:
-        __test_single_token(token[0], token[1], None, 1)
